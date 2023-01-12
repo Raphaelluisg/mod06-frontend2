52ceb34a913bc68f23997e14563d8d86a85f4e7b
Raphaelluisg <raphaelluisg@gmail.com>
2023-01-12 12:03:08 -0300

    feat: add base structure

---
+++ psnViteProject/src/components/pages/home.tsx
@@ -0,0 +1,15 @@
+export function Home() {
+    let number = 0;
+
+    function add() {
+        number++;
+        console.log(number);
+    }
+
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button className="button" onClick={add}> Add Number</button>
+        </div>
+    );
+}
