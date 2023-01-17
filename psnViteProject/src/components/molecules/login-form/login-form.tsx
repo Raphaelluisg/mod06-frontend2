d632a3a3c9aa816be9487edc8c3af075f46a7d21
Raphaelluisg <raphaelluisg@gmail.com>
2023-01-17 20:28:22 -0300

    style: make style from login from component

--- psnViteProject/src/components/molecules/login-form/login-form.tsx
+++ psnViteProject/src/components/molecules/login-form/login-form.tsx
@@ -1,7 +1,14 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { StyledForm, StyledLoginForm } from "./styles";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
 
 export function LoginForm() {
+
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    function handleShowPassword() {
+        setShowPassword(!showPassword);
+    }
 
     function handleSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -17,8 +24,22 @@ export function LoginForm() {
             <h2>PlayStation</h2>
             <StyledForm onSubmit={handleSubmit}>
                 <input placeholder="ID(Email)" name="email" />
-                <input placeholder="Password" name="password" />
-                <button>Login</button>
+                <div>
+                    <input 
+                        placeholder="Password" 
+                        type={showPassword ? "text" : "password"} 
+                        name="password" 
+                        required
+                    />
+                    <button onClick={handleShowPassword}>
+                        {showPassword ? ( 
+                            <BsFillEyeSlashFill size={23} />
+                        ) : ( 
+                            <BsFillEyeFill size={23} />
+                        )}
+                    </button>
+                </div>
+                <button type="submit">Login</button>
             </StyledForm>
         </StyledLoginForm>
     );
--- psnViteProject/src/components/molecules/login-form/styles.ts
+++ psnViteProject/src/components/molecules/login-form/styles.ts
@@ -28,9 +28,49 @@ export const StyledForm = styled.form`
         padding: 3px 5px;
         margin: 8px;
         border-radius: 6px;
+        border: none;
+        height: 35px;
+    }
+
+    div {
+        display: flex;
+        width: 90%;
+        margin: 8px;
+        border-radius: 6px;
+        flex-direction: row;
+        border: none;
+        align-items: center;
+
+        input {
+        display: flex;
+        width: 100%;
+        border: none;
+        padding: 3px 5px;
+        margin: 0;
+        border-radius: 6px 0 0 6px;
+        height: 35px;
+        }
+
+        button {
+            display: flex;
+            align-items: center;
+            border: none;
+            margin: 0;
+            padding: 0;
+            cursor: pointer;
+            border-radius: 0 6px 6px 0;
+            height: 35px;
+        }
     }
 
     button {
-        margin: 5px;
+        display: flex;
+        align-items: center;
+        border: none;
+        border-radius: 5px;
+        margin: 10px;
+        padding: 5px 10px;
+        background-color: ${(props) => props.theme.darkTheme.fg};
+        cursor: pointer;
     }
 `;
